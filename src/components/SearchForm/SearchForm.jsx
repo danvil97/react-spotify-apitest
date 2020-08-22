4d673180bec0e59b3e057d4821b8f14427a878b3
Danius Vilimas <danvil97@yandex.ru>
2020-08-23 01:50:02 +0300

    Form styling

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import CSSModules from 'react-css-modules';
 
 import FormSelect from '../FormSelect/FormSelect';
-import FormCheckbox from '../FormCheckbox/FormCheckbox';
 import TypeButtons from '../TypeButtons/TypeButtons';
 import SearchField from '../SearchField/SearchField';
 import PrimaryButton from '../PrimaryButton/PrimaryButton';
@@ -14,15 +13,18 @@ import styles from './SearchForm.module.scss';
 const SearchForm = () => {
   return (
     <div styleName='base'>
-      <div styleName='searchBox'>
+      <div styleName='base__firstRow'>
         <SearchField placeholder='search text...' />
         <FormSelect optionsList={SEARCH.limitList} />
-        <PrimaryButton withBorder>
-          <BsSearch />
-        </PrimaryButton>
       </div>
-      <FormCheckbox>Checkbox test</FormCheckbox>
-      <TypeButtons typeList={SEARCH.typeList} />
+      <div styleName='base__secondRow'>
+        <TypeButtons typeList={SEARCH.typeList} />
+        <div styleName='searchButton'>
+          <PrimaryButton withBorder>
+            <BsSearch />
+          </PrimaryButton>
+        </div>
+      </div>
     </div>
   );
 };
