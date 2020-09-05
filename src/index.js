47bf1f88848bd4929b5ae66469a3d26bb563b7e1
Danius Vilimas <danvil97@yandex.ru>
2020-09-05 15:45:01 +0300

    added axios, basic store implementation (redux)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import rootReducer from './store/reducers/rootReducer';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import './index.css';
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
